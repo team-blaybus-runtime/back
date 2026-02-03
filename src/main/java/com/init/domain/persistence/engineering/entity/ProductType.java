ec8d8852edda9b5ab37bcfce484ce86e4a35f61c
CHISANW <keuye06380618@gmail.com>
2026-02-04 03:15:06 +0900

    [Feat] 엔지니어링 지식 및 임베딩 엔티티 클래스 추가
    엔지니어링 지식 데이터 저장을 위한 EngineeringKnowledge 엔티티 추가. 임베딩 벡터 저장을 위한 EngineeringKnowledgeEmbedding 엔티티 포함. JPA 매핑 및 주요 필드에 주석 추가.

---
+++ src/main/java/com/init/domain/persistence/engineering/entity/ProductType.java
@@ -0,0 +1,11 @@
+package com.init.domain.persistence.engineering.entity;
+
+public enum ProductType {
+    Drone,
+    Leaf_Spring,
+    Machine_Vice,
+    Robot_Arm,
+    Robot_Gripper,
+    Suspension,
+    V4_Engine
+}
---
+++ src/main/java/com/init/domain/persistence/vector/entity/EngineeringKnowledgeEmbedding.java
@@ -0,0 +1,26 @@
+package com.init.domain.persistence.vector.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Data
+@Table(name = "engineering_knowledge_embedding")
+@AllArgsConstructor
+@NoArgsConstructor
+public class EngineeringKnowledgeEmbedding {
+
+    @Id
+    @Comment("MySQL의 EngineeringKnowledge ID")
+    private Long knowledgeId;
+
+    @Column(columnDefinition = "vector(1536)")
+    @Comment("임베딩 벡터 (1536차원)")
+    private float[] embedding;
+
+    @Comment("제품 간단 분류")
+    private String productType;
+}
