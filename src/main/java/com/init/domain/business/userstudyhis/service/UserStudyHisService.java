a379a01ad967fc312c800718c6f3353d2709636b
seungheonlee <lsh2613@naver.com>
2026-02-07 18:51:24 +0900

    feat: UserStudyHis 기능 구현

---
+++ src/main/java/com/init/domain/business/userstudyhis/service/UserStudyHisService.java
@@ -0,0 +1,53 @@
+package com.init.domain.business.userstudyhis.service;
+
+import com.init.application.dto.userstudyhis.req.UserStudyHisCreateReq;
+import com.init.application.dto.userstudyhis.res.UserStudyHisReadRes;
+import com.init.domain.business.common.service.EntitySimpReadService;
+import com.init.domain.business.engineering.service.ProductImageUrlMapper;
+import com.init.domain.persistence.engineering.entity.ProductType;
+import com.init.domain.persistence.user.entity.User;
+import com.init.domain.persistence.userstudyhis.entity.UserStudyHis;
+import com.init.domain.persistence.userstudyhis.repository.UserStudyHisRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UserStudyHisService {
+    private final EntitySimpReadService entitySimpReadService;
+    private final UserStudyHisRepository userStudyHisRepository;
+    private final ProductImageUrlMapper productImageUrlMapper;
+
+    @Transactional
+    public void create(Long userId, UserStudyHisCreateReq req) {
+        User user = entitySimpReadService.findUser(userId);
+        UserStudyHis userStudyHis = UserStudyHis.of(
+                user, req.productType(), req.title(), req.viewInfo()
+        );
+
+        userStudyHisRepository.save(userStudyHis);
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserStudyHisReadRes> read(Long userId) {
+        User user = entitySimpReadService.findUser(userId);
+        List<UserStudyHis> userStudyHisList = userStudyHisRepository.findByUser(user);
+
+        return userStudyHisList.stream()
+                .map(userStudyHis -> {
+                    ProductType productType = userStudyHis.getProductType();
+                    return new UserStudyHisReadRes(
+                            userStudyHis.getId(),
+                            userStudyHis.getTitle(),
+                            userStudyHis.getUpdatedAt(),
+                            userStudyHis.getViewInfo(),
+                            productType.getDescription(),
+                            productImageUrlMapper.getImageUrlForType(productType)
+                    );
+                })
+                .toList();
+    }
+}
