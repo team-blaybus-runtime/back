01adb8289e36c6876f32ec8783c6d588657f2c84
LSH <lsh2613@naver.com>
2026-02-09 11:09:48 +0900

    feat: workflow 엔티티 설계

---
+++ src/main/java/com/init/domain/persistence/worflow/entity/WorkFlow.java
@@ -0,0 +1,49 @@
+package com.init.domain.persistence.worflow.entity;
+
+
+import com.init.domain.persistence.common.model.DateAuditable;
+import com.init.domain.persistence.user.entity.User;
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.Type;
+
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "work_flow")
+@Entity
+public class WorkFlow extends DateAuditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "work_flow_id")
+    private Long id;
+
+    @Comment("회원 정보")
+    @ManyToOne
+    private User user;
+
+    @Comment("학습 내용 제목")
+    private String title;
+
+    @Type(JsonType.class)
+    @Column(name = "node_info", columnDefinition = "text")
+    private Map<String, Object> nodeInfo;
+
+    public static WorkFlow of(User user, String title, Map<String, Object> nodeInfo) {
+        WorkFlow workFlow = new WorkFlow();
+        workFlow.user = user;
+        workFlow.title = title;
+        workFlow.nodeInfo = nodeInfo;
+        return workFlow;
+    }
+
+    public void update(String title, Map<String, Object> nodeInfo) {
+        this.title = title;
+        this.nodeInfo = nodeInfo;
+    }
+}
